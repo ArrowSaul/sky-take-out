500db4c1336ceb0b933759fa985dbae337f45878
WangShuo <2192595668@qq.com>
2024-03-26 23:24:00 +0800

    批量删除菜品功能代码优化

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -10,6 +10,8 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface DishMapper {
 
@@ -31,4 +33,7 @@ public interface DishMapper {
     //根据主键删除菜品数据
     @Delete("delete from dish where id =#{id}")
     void deleteById(Long id);
+    //根据菜品id集合批量删除菜品数据
+    void deleteByIds(List<Long> ids);
+
 }
