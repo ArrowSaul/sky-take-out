6703ad62995b1e93c2cbf4e34fa7b7f83c3bf806
wangshuo <2192595668@qq.com>
2025-02-27 10:15:09 +0800

    订单支付功能代码

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -7,8 +7,19 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
+    /**
+     * 根据openid查询用户
+     * @param openid
+     * @return
+     */
     @Select("select * from user where openid=#{openid}")
     User getByOpenid(String openid);
 
+    /**
+     * 根据用户id查询用户
+     * @param user
+     */
     void insert(User user);
+    @Select("select * from user where id=#{userId}")
+    User getById(Long userId);
 }
