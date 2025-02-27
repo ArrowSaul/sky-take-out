cf0a4ba7284924f67d35fcc4a8dbad547c137ead
wangshuo <2192595668@qq.com>
2025-02-27 16:27:19 +0800

    拒单功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -2,6 +2,7 @@ package com.sky.controller.admin;
 
 import com.sky.dto.OrdersConfirmDTO;
 import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.dto.OrdersRejectionDTO;
 import com.sky.entity.Orders;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
@@ -63,13 +64,25 @@ public class OrderController {
 
     /**
      * 接单
-     * @param id
+     * @param ordersConfirmDTO
      * @return
      */
     @PutMapping("/confirm")
     public Result confirm(@RequestBody OrdersConfirmDTO ordersConfirmDTO) {
         log.info("接单:{}", ordersConfirmDTO);
         orderService.confirm(ordersConfirmDTO);
+        return Result.success();
+    }
+    /**
+     * 拒单
+     * @param ordersRejectionDTO
+     * @return
+     * @throws Exception
+     */
+    @PutMapping("/rejection")
+    @ApiOperation("拒单")
+    public Result rejection(@RequestBody OrdersRejectionDTO ordersRejectionDTO) throws Exception {
+        orderService.rejection(ordersRejectionDTO);
         return Result.success();
     }
 }
