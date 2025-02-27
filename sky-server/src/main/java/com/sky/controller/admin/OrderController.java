484ffd4156a0315cd2bc3229098f1209b1cca5ee
wangshuo <2192595668@qq.com>
2025-02-27 16:08:11 +0800

    订单搜索功能代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -0,0 +1,51 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.OrderStatisticsVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * 订单管理
+ */
+@RestController
+@RequestMapping("/admin/order")
+@Slf4j
+@Api(tags = "订单相关接口")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+    /**
+     * 订单搜索
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    @GetMapping("/conditionSearch")
+    @ApiOperation("订单搜索")
+    public Result<PageResult> page(OrdersPageQueryDTO ordersPageQueryDTO) {
+        log.info("订单搜索:{}", ordersPageQueryDTO);
+        PageResult pageResult = orderService.conditionSearch(ordersPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+//    /**
+//     * 各状态订单数量统计
+//     * @return
+//     */
+//    @GetMapping("/statistics")
+//    @ApiOperation("各状态订单数量统计")
+//    public Result<OrderStatisticsVO> statistics() {
+//        log.info("各状态订单数量统计");
+//        OrderStatisticsVO orderStatisticsVO = orderService.statistics();
+//        return Result.success(orderStatisticsVO);
+//    }
+}
